2dd6e86bb9847cf6756029197204586587045bdd
MagentaCoding <emergency.berlin@outlook.de>
2025-12-01 09:10:45 +0100

    Create fsharp.h for F# support in MagentaOS
    Add F# support header file for MagentaOS.

---
+++ programminglanguagesupport/fsharp.h
@@ -0,0 +1,6 @@
+// 
+// MagentaOS Library – Programming Language Support
+// MagentaOS F# Support
+// Programming Support Library for F# Support
+// This library is mainly used for and by the csharp.c
+// 
