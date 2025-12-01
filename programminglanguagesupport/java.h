f22158ceb085f4c13bbd55efb4f68548a5646de4
MagentaCoding <emergency.berlin@outlook.de>
2025-12-01 09:04:29 +0100

    Add purpose comment to java.h
    Add a comment explaining the purpose of the library.

--- programminglanguagesupport/java.h
+++ programminglanguagesupport/java.h
@@ -2,4 +2,5 @@
 // MagentaOS Library – Programming Language Support
 // MagentaOS Java Support
 // Programming Language Support Library for Java
+// This library is used for the Programming Language Support in the file java.c
 //
